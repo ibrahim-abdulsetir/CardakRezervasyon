8023b375744703e2b9e5872baed1a755c50029e5
Ibrahim Abdulsetir <ibrahimabdulsetir@gmail.com>
2026-07-02 20:04:20 +0300

    Add project folder structure and MesireAlani entity

---
+++ CardakRezervasyon.Api/Models/Entities/Cardak.cs
@@ -0,0 +1,11 @@
+﻿namespace CardakRezervasyon.Api.Models.Entities
+{
+    public class Cardak
+    {
+        public int Id { get; set; }
+        public int MesireAlaniId { get; set; }
+
+        // We'll add the rest of the fields (Numara, Blok, Kapasite, etc.)
+        // in the next step — this is just enough to make MesireAlani compile.
+    }
+}
---
+++ CardakRezervasyon.Api/Models/Entities/MesireAlani.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardakRezervasyon.Api.Models.Entities
+{
+    public class MesireAlani
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; } = string.Empty;
+        public string? Aciklama { get; set; }
+        public string Mahalle { get; set; } = string.Empty;
+        public TimeSpan AcilisSaati { get; set; }
+        public TimeSpan KapanisSaati { get; set; }
+        public bool AktifMi { get; set; } = true;
+        public DateTime OlusturmaTarihi { get; set; } = DateTime.UtcNow;
+
+        // Navigation property: one park has many çardaks
+        public ICollection<Cardak> Cardaklar { get; set; } = new List<Cardak>();
+    }
+}
