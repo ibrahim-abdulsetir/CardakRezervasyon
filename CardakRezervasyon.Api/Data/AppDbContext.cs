1f7a7c246d7171ce707d06692f2d26aec8976ad9
Ibrahim Abdulsetir <ibrahimabdulsetir@gmail.com>
2026-07-02 16:24:44 +0300

    Add EF Core SQL Server packages, connection string, and empty DbContext

---
+++ CardakRezervasyon.Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CardakRezervasyon.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // We'll add DbSet<T> properties here once we create our entities (next milestone)
+    }
+}
