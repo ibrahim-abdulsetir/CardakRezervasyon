218d8920e2fc7472bdfd2c46c7acf49e745f1459
Ibrahim Abdulsetir <ibrahimabdulsetir@gmail.com>
2026-07-03 05:59:39 +0300

    Add full cardak entity with relationship to MesirreAlani

--- CardakRezervasyon.Api/Models/Entities/Cardak.cs
+++ CardakRezervasyon.Api/Models/Entities/Cardak.cs
@@ -1,11 +1,20 @@
-﻿namespace CardakRezervasyon.Api.Models.Entities
+﻿using System.Collections.Generic;
+
+namespace CardakRezervasyon.Api.Models.Entities
 {
     public class Cardak
     {
         public int Id { get; set; }
-        public int MesireAlaniId { get; set; }
 
-        // We'll add the rest of the fields (Numara, Blok, Kapasite, etc.)
-        // in the next step — this is just enough to make MesireAlani compile.
+        public int MesireAlaniId { get; set; }
+        public MesireAlani MesireAlani { get; set; } = null!;
+
+        public int Numara { get; set; }
+        public string? Blok { get; set; }
+        public int Kapasite { get; set; }
+        public bool MangalliMi { get; set; }
+        public bool AktifMi { get; set; } = true;
+
+        public ICollection<Rezervasyon> Rezervasyonlar { get; set; } = new List<Rezervasyon>();
     }
 }
---
+++ CardakRezervasyon.Api/Models/Entities/Rezervasyon.cs
@@ -0,0 +1,10 @@
+﻿namespace CardakRezervasyon.Api.Models.Entities
+{
+    public class Rezervasyon
+    {
+        public int Id { get; set; }
+        public int CardakId { get; set; }
+
+        // Full fields coming in the next step
+    }
+}
